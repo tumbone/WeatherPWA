c4c016f495b09d94d395cbcf7913e0b0ae5eb8b3
tumbonea <tumbonea@ovationsgroup.com>
2018-08-26 21:54:13 +0200

    Update SW variable declarations and arrow functions

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-var cacheName = 'weatherPWA-v3';
-var dataCacheName = 'weatherData-v1';
+const cacheName = 'weatherPWA-v3';
+const dataCacheName = 'weatherData-v1';
 
-var filesToCache = [
+const filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
@@ -27,21 +27,21 @@ var filesToCache = [
   '/images/wind.png'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then((cache) => {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   )
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', (event) => {
   console.log('[ServiceWorker] Activate');
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
@@ -52,9 +52,9 @@ self.addEventListener('activate', function (event) {
   return self.clients.claim();
 })
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', (e) => {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  const dataUrl = 'https://query.yahooapis.com/v1/public/yql';
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
@@ -64,8 +64,8 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
      */
     e.respondWith(
-      caches.open(dataCacheName).then(function (cache) {
-        return fetch(e.request).then(function (response) {
+      caches.open(dataCacheName).then((cache) => {
+        return fetch(e.request).then((response) => {
           cache.put(e.request.url, response.clone());
           return response;
         });
@@ -78,7 +78,7 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
     e.respondWith(
-      caches.match(e.request).then(function (response) {
+      caches.match(e.request).then((response) => {
         return response || fetch(e.request);
       })
     );
